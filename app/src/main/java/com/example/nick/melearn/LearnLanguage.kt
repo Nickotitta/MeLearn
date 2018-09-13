ecd4d2108a4a1690d641d0046b1d617d5da71bc4
Nickotitta <nick.frejneman@gmail.com>
2018-09-13 13:47:01 +0200

    Added some new activites

--- app/src/main/java/com/example/nick/melearn/LearnLanguage.kt
+++ app/src/main/java/com/example/nick/melearn/LearnLanguage.kt
@@ -3,14 +3,10 @@ package com.example.nick.melearn
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LearnLanguage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val actionBar = supportActionBar
-        actionBar!!.hide()
-        
+        setContentView(R.layout.activity_learn_language)
     }
 }
